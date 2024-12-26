5b4841cbd2e68d90ef28f50dc1f86788d78732d9
trrrrr617 <tr050617@163.com>
2024-12-26 18:18:34 +0800

    Add files via upload

--- data_create_basic.py
+++ data_create_basic.py
@@ -20,7 +20,7 @@ def answer_generate(message):
         "messages": [
             {
                 "role": "user",
-                "content": "介绍一下北京"
+                "content": message
             }
         ]
     })
@@ -28,7 +28,7 @@ def answer_generate(message):
         'Content-Type': 'application/json'
     }
     response = requests.request("POST", url, headers=headers, data=payload)
-    return response.text
+    return response.json().get("result")
 
 
 def get_nodes(request):   # 1为颜色，2为公司，3为需求
@@ -70,7 +70,8 @@ if __name__ == "__main__":
                     question = "请基于"+emotion[j]+"情感提出一个关于你需要选配空调的诉求，诉求的目的是使得空调有以下属性："
                     question = question+str(node)
                     question = question+("。目前已经有过如下的结果："+"".join(history) +
-                                         "。请展开思路，写一个与众不同的回复。同时，我希望其中不要参杂表情包等内容，大约40字以内即可，请使用一些口语化的词进行生成！而不是过于追求华丽、优美、对仗")
+                                         "。请展开思路，写一个与众不同的提问。同时，我希望其中不要参杂表情包等内容，大约40字以内即可，请"
+                                         "使用一些口语化的词进行生成！而不是过于追求华丽、优美、对仗。注意不要写的跟宣传口号那样！要写的更像用户正常的提问诉求")
                     print("如下是给大模型进行的提问："+question)
                     answer = answer_generate(question)
                     print("以下是给出的答复："+answer)
