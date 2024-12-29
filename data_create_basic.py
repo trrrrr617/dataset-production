1536e5ed1b16c0aaf83fa219ceec5a3cde5e1a38
trrrrr617 <tr050617@163.com>
2024-12-29 16:53:05 +0800

    Add files via upload

--- data_create_basic.py
+++ data_create_basic.py
@@ -4,7 +4,8 @@ import requests
 
 
 key = ("https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=DXR"
-       "[your key]&client_secret=[put your secret here]")
+       "8hnVsIcH7Z0SQrx56TnM6&client_secret=53uI4Qek8y0jDAzmABgZvbZnU2ryNESN"
+       "")
 
 
 def answer_generate(message):
@@ -31,7 +32,7 @@ def answer_generate(message):
     return response.json().get("result")
 
 
-def get_nodes(request):
+def get_nodes(request):   # 1为颜色，2为公司，3为需求
     getting_nodes = []
     if request == 1:
         with open('basic_Color.json', 'r', encoding="utf-8") as f:
@@ -57,27 +58,30 @@ if __name__ == "__main__":
     for i in range(1, 4):
         dic = {}
         try:
+            # 获取所有LowPrice标签的节点
             nodes = get_nodes(i)
             print("Found {} nodes:".format(len(nodes)))
             count = 0
             for node in nodes:
                 count += 1
                 generating = []
-                emotion = ["开心", "轻松", "中立", "愤怒", "害羞", "紧张", "奇怪", "焦虑", "急躁", "激动"]
+                emotion = ["开心","平静", "焦虑", "急切"]
+                money=["较差","尚可", "很好"]
                 history = []
-                for j in range(10):
-                    question = "请基于"+emotion[j]+"情感提出一个关于你需要选配空调的诉求，诉求的目的是使得空调有以下属性："
-                    question = question+str(node)
-                    question = question+("。目前已经有过如下的结果："+"".join(history) +
-                                         "。请展开思路，写一个与众不同的提问。同时，我希望其中不要参杂表情包等内容，大约40字以内即可，请"
-                                         "使用一些口语化的词进行生成！而不是过于追求华丽、优美、对仗。注意不要写的跟宣传口号那样！要写的更像用户正常的提问诉求")
-                    print("如下是给大模型进行的提问："+question)
-                    answer = answer_generate(question)
-                    print("以下是给出的答复："+answer)
-                    history.append(answer)
-                    generating.append(answer)
-                    print("正在执行第"+str(i)+"个类别（颜色、公司、需求）中的第"+str(count)+"个关键词")
-                dic[str(node)] = generating
+                for j in range(4):
+                    for k in range(3):
+                        question = "请基于"+emotion[j]+"情感提出一个关于你需要选配空调的诉求，表现出提出诉求的人经济条件"+money[k]+"不要直接写出经济条件，诉求的目的是使得空调有以下属性："
+                        question = question+str(node)
+                        question = question+("。目前已经有过如下的结果："+"".join(history) +
+                                             "。请展开思路，写一个与众不同的提问。同时，我希望其中不要参杂表情包等内容，大约40字以内即可，请"
+                                             "使用一些口语化的词进行生成！而不是过于追求华丽、优美、对仗。注意不要写的跟宣传口号那样！要写的更像用户正常的提问诉求")
+                        print("如下是给大模型进行的提问："+question)
+                        answer = answer_generate(question)
+                        print("以下是给出的答复："+answer)
+                        history.append(answer)
+                        generating.append(answer)
+                        print("正在执行第"+str(i)+"个类别（颜色、公司、需求）中的第"+str(count)+"个关键词")
+                    dic[str(node)] = generating
         finally:
             path = " "
             if i == 1:
